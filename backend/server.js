c23a19c5f81ee46a0ed74ebd6d720552a3a10a25
Devansh <156525808+Devansh3067@users.noreply.github.com>
2026-01-01 22:00:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5001",
+        origin: "https://compile-crew.onrender.com",
         methods: ["GET", "POST"],
         credentials: true
     }

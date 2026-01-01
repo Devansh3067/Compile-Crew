75f59265652af28b2aea27bc5ad30592ea9f88d3
Devansh <156525808+Devansh3067@users.noreply.github.com>
2026-01-01 21:46:54 +0530

    Update EditorPage.jsx

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -98,7 +98,7 @@ const EditorPage = () => {
         setReview("## Ai is evaluating your code...");
 
         try {
-            const response = await axios.post('http://localhost:5001/ai/get-review', { code });
+            const response = await axios.post('https://compile-crew.onrender.com/ai/get-review', { code });
             // console.log(response.data)
             setReview(response.data);
         } catch (error) {
